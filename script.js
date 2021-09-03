0cdd6f25570af67f887da0bc7e177c0e90409af3
molotskyb <molotskyb@gmail.com>
2021-09-03 16:39:15 +0300

    nav sticky add commit

--- script.js
+++ script.js
@@ -2,55 +2,55 @@
 
 ///////////////////////////////////////
 // Slider
-const slider = function () {
-	const slides = document.querySelectorAll(".slide");
-	const btnLeft = document.querySelector(".slider__btn--left");
-	const btnRight = document.querySelector(".slider__btn--right");
+// const slider = function () {
+// 	const slides = document.querySelectorAll(".slide");
+// 	const btnLeft = document.querySelector(".slider__btn--left");
+// 	const btnRight = document.querySelector(".slider__btn--right");
 
-	let curSlide = 0;
-	const maxSlide = slides.length;
+// 	let curSlide = 0;
+// 	const maxSlide = slides.length;
 
-	// Functions
+// 	// Functions
 
-	const goToSlide = function (slide) {
-		slides.forEach(
-			(s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-		);
-	};
+// 	const goToSlide = function (slide) {
+// 		slides.forEach(
+// 			(s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+// 		);
+// 	};
 
-	// Next slide
-	const nextSlide = function () {
-		if (curSlide === maxSlide - 1) {
-			curSlide = 0;
-		} else {
-			curSlide++;
-		}
+// 	// Next slide
+// 	const nextSlide = function () {
+// 		if (curSlide === maxSlide - 1) {
+// 			curSlide = 0;
+// 		} else {
+// 			curSlide++;
+// 		}
 
-		goToSlide(curSlide);
-	};
+// 		goToSlide(curSlide);
+// 	};
 
-	const prevSlide = function () {
-		if (curSlide === 0) {
-			curSlide = maxSlide - 1;
-		} else {
-			curSlide--;
-		}
-		goToSlide(curSlide);
-	};
+// 	const prevSlide = function () {
+// 		if (curSlide === 0) {
+// 			curSlide = maxSlide - 1;
+// 		} else {
+// 			curSlide--;
+// 		}
+// 		goToSlide(curSlide);
+// 	};
 
-	const init = function () {
-		goToSlide(0);
-	};
-	init();
+// 	const init = function () {
+// 		goToSlide(0);
+// 	};
+// 	init();
 
-	// Event handlers
-	btnRight.addEventListener("click", nextSlide);
-	btnLeft.addEventListener("click", prevSlide);
+// 	// Event handlers
+// 	btnRight.addEventListener("click", nextSlide);
+// 	btnLeft.addEventListener("click", prevSlide);
 
-	document.addEventListener("keydown", function (e) {
-		if (e.key === "ArrowLeft") prevSlide();
-		e.key === "ArrowRight" && nextSlide();
-	});
-};
-slider();
+// 	document.addEventListener("keydown", function (e) {
+// 		if (e.key === "ArrowLeft") prevSlide();
+// 		e.key === "ArrowRight" && nextSlide();
+// 	});
+// };
+// slider();
 ///////////////////////////////////////
